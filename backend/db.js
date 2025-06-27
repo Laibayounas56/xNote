cd61c83f956c217c4752577bea09d23bcb1bc0a1
Laibayounas56 <laibay517@gmail.com>
2025-06-27 14:18:58 +0500

    mongoDB connection

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const connectToMongo = () => {
+  mongoose.connect("mongodb://localhost:27017/yourdbname")
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log("Connection error:", err));
+};
+
+module.exports = connectToMongo;
