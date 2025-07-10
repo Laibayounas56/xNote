523dccd51a5121b233e5d137b394827479028b36
Laibayounas56 <laibay517@gmail.com>
2025-07-10 22:58:47 +0500

    vercel "/" route

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,9 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
+app.get('/', (req, res) => {
+  res.send('xNote backend is up and running!');
+});
 
 app.listen(port, () => {
   console.log(`xNote backend listening at http://localhost:${port}`)
