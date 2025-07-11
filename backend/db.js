1aa8d24429872ea983ea1e43ca8a33085f90341a
Laibayounas56 <laibay517@gmail.com>
2025-07-11 18:49:23 +0500

    connection change

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,24 @@
 const mongoose = require("mongoose");
 
-const connectToMongo = () => {
- mongoose.connect("mongodb+srv://xNote:xNote578@cluster0.33egmgq.mongodb.net/xNote?retryWrites=true&w=majority&appName=Cluster0")
-    .then(() => console.log(" MongoDB Atlas connected"))
-    .catch((err) => console.error(" Connection error:", err));
+let isConnected = false;
+
+const connectToMongo = async () => {
+  if (isConnected) {
+    console.log(" MongoDB already connected");
+    return;
+  }
+
+  try {
+    await mongoose.connect("mongodb+srv://xNote:xNote578@cluster0.33egmgq.mongodb.net/xNote?retryWrites=true&w=majority&appName=Cluster0", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log(" MongoDB Atlas connected");
+  } catch (err) {
+    console.error(" MongoDB connection error:", err);
+    throw err;
+  }
 };
 
 module.exports = connectToMongo;
