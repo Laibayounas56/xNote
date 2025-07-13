bbefe94d86febe52d9ba5d9f12ebf6c2d6eb5da8
Laibayounas56 <laibay517@gmail.com>
2025-07-13 20:16:17 +0500

    fixes

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,21 @@ const express = require('express');
 const cors = require('cors');
 const serverless = require('serverless-http');
 
-connectToMongo();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
 app.get('/', (req, res) => {
   res.send('xNote backend is up and running!');
 });
-module.exports = serverless(app);
 
+(async () => {
+  await connectToMongo();
+  console.log(' MongoDB connected. Server is ready.');
+})();
+
+module.exports = serverless(app);
